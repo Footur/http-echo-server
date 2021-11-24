4a9d0b437e4a3443db56bb977e11f4aa6b41168f
Tim Bultmann <tim.bultmann@govnernikus.de>
2021-11-24 11:09:44 +0100

    Remove docker commands from build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
 mvn clean package
-
-docker build --network=host --build-arg TAG=$1 -t kosprov/http-echo-server:$1 .
-docker tag kosprov/http-echo-server:$1 kosprov/http-echo-server:latest
